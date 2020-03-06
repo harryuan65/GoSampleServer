71130bb1b6cc5481fc204311d5360cd7f61836db
Harry Yuan <harryuan.65@gmail.com>
2020-03-06 13:35:41 +0800

    add simple params

--- server.go
+++ server.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"html"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"time"
 )
 
 // func test(rw http.ResponseWriter, req *http.Request) {
@@ -21,10 +25,39 @@ import (
 // 	log.Println(t)
 // }
 
+func check(err interface{}) {
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
 func main() {
 	http.HandleFunc("/p", func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(rw, "Hello, %q", html.EscapeString(r.URL.Path))
 		log.Println(r.URL.Path)
+		var m map[string]interface{}
+		m = make(map[string]interface{})
+		m["user_id"] = 123
+		m["token"] = "ABCDEF"
+
+		requestBody, err := json.Marshal(m)
+		check(err)
+
+		timeout := time.Duration(5 * time.Second)
+		client := http.Client{
+			Timeout: timeout,
+		}
+		request, err := http.NewRequest("POST", "http://localhost:3005/any", bytes.NewBuffer(requestBody))
+		request.Header.Set("Content-Type", "application/json")
+		check(err)
+
+		resp, err := client.Do(request)
+		check(err)
+
+		defer resp.Body.Close()
+
+		body, err := ioutil.ReadAll(resp.Body)
+		check(err)
+		log.Println(string(body))
 	})
 	port := ":8888"
 	log.Printf("Start Server at %s\n", port)
